caf91f1f22b24838431688631ae758edb3a6a344
jjgalan <jjgalandelgad@gmail.com>
2020-06-12 00:31:48 -0500

    reto

---
+++ target/classes/import.sql
@@ -0,0 +1,5 @@
+insert into currency (iso, name, exchange) values ('USD', 'Dolar estadounidense', 1);
+insert into currency (iso, name, exchange) values ('PEN', 'Sol', 3.43);
+insert into currency (iso, name, exchange) values  ('EUR', 'Euro', 0.88);
+insert into currency (iso, name, exchange) values ('RUB', 'Rublo', 70.14);
+insert into currency (iso, name, exchange) values ('GBP', 'Libra esterlina', 0.79);
